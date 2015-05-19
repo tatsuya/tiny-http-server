cf3f91adf480b667408fc86c1e4966fdb17becd9
Gil Casadevall <gil@ixde.net>
2015-05-19 16:58:24 +0200

    Added support to serve a file content as http body

--- tiny-http-server.js
+++ tiny-http-server.js
@@ -1,23 +1,28 @@
 #!/usr/bin/env node
+'use strict';
 
 var commander = require('commander');
+var fs = require('fs');
 
 commander
   .option('-p, --port [num]', 'port number the server listen to')
   .option('-s, --status [code]', 'http status code')
   .option('-b, --body [content]', 'http body content')
   .option('-c, --content-type [value]', 'Content-Type header value')
+  .option('-f, --file [path]', 'file to serve as http body content')
   .parse(process.argv);
 
 var port = commander.port || 3333;
 var status = commander.status || 200;
-var body = commander.body || '';
+var filePath = commander.file || '';
+var fileContent = filePath ? fs.readFileSync(filePath) : '';
+var body = commander.body || fileContent;
 var contentType = commander.contentType || 'text/plain';
 
 var http = require('http');
 
 var server = http.createServer(function(req, res) {
-  console.log('[%s] "%s %s" "%s"', (new Date).toUTCString(), req.method, req.url, req.headers['user-agent']);
+  console.log('[%s] "%s %s" "%s"', (new Date()).toUTCString(), req.method, req.url, req.headers['user-agent']);
   res.setHeader('Content-Length', body.length);
   res.setHeader('Content-Type', contentType);
   res.statusCode = status;
